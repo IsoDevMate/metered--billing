f325772f1b3f2efd6fc57fad5559db62a08b234f
IsoDevMate <oumaoduor5827@gmail.com>
2024-05-18 14:37:57 +0300

    Services incoperated to MVC Architecture: added the storage stripe and download servuces : serices folder

--- backend/controllers/firebase/downloadcontroller.js
+++ backend/controllers/firebase/downloadcontroller.js
@@ -1,66 +1,47 @@
-const admin = require('firebase-admin');
 const User = require('../../models/schema');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-require("dotenv").config();
-//const path = require('path');
-
+const { getFileDownloadLink } = require('../../services/storageservice');
+const {
+    listOutstandingInvoices,
+    retrieveUpcomingInvoice,
+    createInvoice,
+    createCheckoutSession,
+  } = require('../../services/stripedownloadsevice');
 exports.downloadpaidfile = async (req, res) => {
-        try {
-          const fileId = req.query.fileId;
-          const fileName = req.query.fileName;
-          const firebaseUid = req.query.firebaseUid;
-      
-          const user = await User.findOne({ firebaseUid: firebaseUid });
-          if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-          }
-      
-          let outstandingInvoices = await stripe.invoices.list({
-            customer: user.stripeCustomerId,
-            status: 'open',
-          });
-      
-      
-          const upcomingInvoice = await stripe.invoices.retrieveUpcoming({
-            customer: user.stripeCustomerId,
-       
-          });
-          console.log("upcomingInvoice",upcomingInvoice)
-       
-          if (upcomingInvoice.total > 0) {
-            const newInvoice = await stripe.invoices.create({
-              customer: user.stripeCustomerId,
-            });
-            outstandingInvoices.data.push(newInvoice);
-          }
-      
-          if (outstandingInvoices.data.length > 0) {
-            const session = await stripe.checkout.sessions.create({
-              payment_method_types: ['card'],
-              mode: 'payment',
-              customer: user.stripeCustomerId,
-              line_items: outstandingInvoices.data.map((invoice) => ({
-                price_data: {
-                  currency: 'usd',
-                  product_data: {
-                    name: 'Outstanding Invoice',
-                  },
-                  unit_amount: Math.round(invoice.amount_due * 100),
-                },
-                quantity: 1,
-              })),
-              success_url: `${process.env.DOMAIN}/success`,
-              cancel_url: `${process.env.DOMAIN}/cancel`,
-            });
-            res.json({ outstandingInvoices: outstandingInvoices.data, checkoutUrl: session.url });
-          } else {
-            const fileRef = admin.storage().bucket().file(`uploads/${fileName}`);
-            const downloadLink = await fileRef.getSignedUrl({ action: 'read', expires: '03-09-2491' });
-            res.json({ downloadLink: downloadLink[0] });
-            console.log("downloadLink",downloadLink)
-          }
-        } catch (error) {
-          console.error('Error initiating download:', error);
-          res.status(500).json({ message: 'Internal server error' });
-        }
+  try {
+    const fileId = req.query.fileId;
+    const fileName = req.query.fileName;
+    const firebaseUid = req.query.firebaseUid;
+
+    const user = await User.findOne({ firebaseUid: firebaseUid });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    let outstandingInvoices = await listOutstandingInvoices(user.stripeCustomerId);
+    const upcomingInvoice = await retrieveUpcomingInvoice(user.stripeCustomerId);
+    console.log("upcomingInvoice", upcomingInvoice);
+
+    if (upcomingInvoice.total > 0) {
+      const newInvoice = await createInvoice(user.stripeCustomerId);
+      outstandingInvoices.data.push(newInvoice);
+    }
+
+    if (outstandingInvoices.data.length > 0) {
+      const session = await createCheckoutSession(user.stripeCustomerId, outstandingInvoices);
+      res.json({ outstandingInvoices: outstandingInvoices.data, checkoutUrl: session.url });
+    } else {
+      try {
+        const downloadLink = await getFileDownloadLink(fileName);
+        res.json({ downloadLink });
+        console.log('downloadLink', downloadLink);
+      } catch (error) {
+        console.error('Error initiating download:', error);
+        res.status(500).json({ message: 'Internal server error' });
       }
+    }
+  } catch (error) {
+    console.error('Error initiating download:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,9 +4,10 @@ const { registerusers } = require("../controllers/firebase/registercontroller")
 const { uploads  } = require("../controllers/firebase/uploadcontroller")
 const { deleteusers } = require("../controllers/stripe/deleteusercontroller")
 const { downloadpaidfile } = require("../controllers/firebase/downloadcontroller")
-router.post('/upload', uploads)
-router.delete('/users/:userId', deleteusers)
-router.get('/download', downloadpaidfile)
+
 router.post('/users', registerusers)
+router.delete('/users/:userId', deleteusers)
+router.post('/upload', uploads)
+router.get('/download', downloadpaidfile)
 
 module.exports= router;
