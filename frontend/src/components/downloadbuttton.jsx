4dbb53789a032cb8888544439ea6db8c0d156db2
IsoDevMate <oumaoduor5827@gmail.com>
2024-05-20 13:24:46 +0300

    Download button from material teilwind react: downloadbutton.jsx

--- frontend/src/components/downloadbuttton.jsx
+++ frontend/src/components/downloadbuttton.jsx
@@ -1,6 +1,6 @@
 
-import { Spinner } from "@material-tailwind/react";
-import { Button } from "@mui/material";
+import { Spinner,Button  } from "@material-tailwind/react";
+
 export const DownloadButtton = ({ onClick,isLoading }) => (
   <Button
     variant="contained"
